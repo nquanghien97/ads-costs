3436fda78208d9f6c29b89e2c84e579a90ad63ae
QuangHien <nquanghien97@gmail.com>
2024-06-28 16:02:36 +0700

    init commit

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,16 @@
+function Login() {
+  return (
+    <div className="bg-[url('./assets/background-login.jpg')] w-screen h-screen bg-center">
+      <div>
+        <div>
+          <p>Đăng nhập</p>
+        </div>
+        <form>
+          
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
