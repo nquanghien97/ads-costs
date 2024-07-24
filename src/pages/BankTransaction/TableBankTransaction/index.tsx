61112aa7941b56d65a107018b79b7b02cf78bcd2
nquanghien97 <nquanghien97@gmail.com>
2024-07-24 17:24:36 +0700

    fix: name sub header

--- src/pages/BankTransaction/TableBankTransaction/index.tsx
+++ src/pages/BankTransaction/TableBankTransaction/index.tsx
@@ -48,7 +48,6 @@ function TableBankTransaction(props: TableBankTransactionProps) {
     <div className="my-8">
       <div className="flex gap-2">
         <div className="flex-[0_0_60%] w-full">
-          <div className="px-6 py-2 my-2 rounded-full h-10 text-white top-0 z-50 flex"></div>
           <ConfigProvider
             theme={{
               token: {
@@ -73,7 +72,7 @@ function TableBankTransaction(props: TableBankTransactionProps) {
               bordered
               scroll={{ x: 2000, y: 240 }}
               rowHoverable={false}
-              rowClassName={(record) => `${record.system.name}`}
+              rowClassName={(record) => `${record.system.name} - ${record.group_datas.flatMap(item => item.group.name)}`}
               components={{
                 body: {
                   row: (({ children, className }: { children: React.ReactNode, className: string})=> <CustomRow children={children} className={className} />),
