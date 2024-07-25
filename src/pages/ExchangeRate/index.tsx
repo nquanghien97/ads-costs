6e460707628ece104f25c7310477b553a31a01e2
nquanghien97 <nquanghien97@gmail.com>
2024-07-25 16:57:53 +0700

    implement: exchange-rate page

--- src/pages/ExchangeRate/index.tsx
+++ src/pages/ExchangeRate/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
-function index() {
+function ExchangeRate() {
   return (
-    <div>index</div>
+    <div className="flex justify-center">
+      <div className="bg-[#0071ba] rounded-md cursor-pointer h-full px-4 py-2 my-4 flex items-center justify-center hover:opacity-80 duration-300">
+        <span className="text-white">Khai báo Chi phí</span>
+      </div>
+    </div>
   )
 }
 
-export default index
+export default ExchangeRate
